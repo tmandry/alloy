07f296a30f7600674be34662dad02caa40f9deb4
Tyler Mandry <tmandry@gmail.com>
2018-07-27 22:11:11 +0200

    Add serial module

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate bootloader_precompiled;
 extern crate lazy_static;
 extern crate spin;
 extern crate volatile;
+extern crate uart_16550;
 
 #[cfg(test)]
 extern crate array_init;
@@ -17,9 +18,11 @@ extern crate std;
 use core::panic::PanicInfo;
 
 #[macro_use]
-mod vga_buffer;
-#[macro_use]
 mod log;
+#[macro_use]
+mod serial;
+#[macro_use]
+mod vga_buffer;
 
 #[cfg(not(test))]
 #[panic_implementation]
@@ -35,6 +38,7 @@ pub extern "C" fn _start() -> ! {
     println!("Hello, world!");
     debug!("2 + 2 = {} and 65.0 / 9.0 = {}", 2 + 2, 65.0 / 9.0);
     error!("Errors look like this");
+    serial_println!("Hello, host! 1 + 2 = {}", 1 + 2);
     info!("Beginning infinite loop...");
     loop {}
 }
