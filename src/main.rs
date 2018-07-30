e582cded0d5cf43d8d1a74e16a31c0647a5f5b86
Tyler Mandry <tmandry@gmail.com>
2018-07-30 23:44:51 +0200

    Add simple exception handler (breakpoint)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 #![feature(panic_implementation)]
+#![feature(abi_x86_interrupt)]
 #![no_std]
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
-use core::panic::PanicInfo;
-
 #[macro_use]
 extern crate alloy;
+
+#[macro_use]
+extern crate lazy_static;
+extern crate x86_64;
+
+use core::panic::PanicInfo;
+use x86_64::structures::idt::{ExceptionStackFrame, InterruptDescriptorTable};
+
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, world!");
+    debug!("2 + 2 = {} and 65.0 / 9.0 = {}", 2 + 2, 65.0 / 9.0);
+    error!("Errors look like this");
+    serial_println!("Hello, host! 1 + 2 = {}", 1 + 2);
+
+    init_idt();
+
+    // Invoke a breakpoint exception
+    x86_64::instructions::int3();
+
+    info!("Shutting down QEMU...");
+    unsafe { alloy::exit_qemu(); }
+
+    loop {}
+}
 
 #[cfg(not(test))]
 #[panic_implementation]
@@ -16,16 +41,18 @@ pub fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[cfg(not(test))]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello, world!");
-    debug!("2 + 2 = {} and 65.0 / 9.0 = {}", 2 + 2, 65.0 / 9.0);
-    error!("Errors look like this");
-    serial_println!("Hello, host! 1 + 2 = {}", 1 + 2);
-    info!("Shutting down QEMU...");
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(handle_breakpoint);
+        idt
+    };
+}
 
-    unsafe { alloy::exit_qemu(); }
+pub fn init_idt() {
+    IDT.load();
+}
 
-    loop {}
+extern "x86-interrupt" fn handle_breakpoint(stack_frame: &mut ExceptionStackFrame) {
+    debug!("EXCEPTION: Breakpoint\n{:#?}", stack_frame);
 }
