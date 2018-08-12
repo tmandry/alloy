0d5087493a1a31a40a960449c2e97e0fe2b41f3e
Tyler Mandry <tmandry@gmail.com>
2018-08-12 18:06:34 +0200

    Add double fault handler and test

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,16 @@ pub extern "C" fn _start() -> ! {
     error!("Errors look like this");
     serial_println!("Hello, host! 1 + 2 = {}", 1 + 2);
 
+    alloy::gdt::init();
     init_idt();
 
     // Invoke a breakpoint exception
     x86_64::instructions::int3();
+
+    fn stack_overflow() {
+        stack_overflow();
+    }
+    stack_overflow();
 
     info!("Shutting down QEMU...");
     unsafe { alloy::exit_qemu(); }
@@ -45,6 +51,12 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(handle_breakpoint);
+
+        unsafe {
+            idt.double_fault.set_handler_fn(handle_double_fault)
+                            .set_stack_index(alloy::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
         idt
     };
 }
@@ -55,4 +67,11 @@ pub fn init_idt() {
 
 extern "x86-interrupt" fn handle_breakpoint(stack_frame: &mut ExceptionStackFrame) {
     debug!("EXCEPTION: Breakpoint\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn handle_double_fault(
+    stack_frame: &mut ExceptionStackFrame, _error_code: u64)
+{
+    error!("EXCEPTION: Double fault\n{:#?}", stack_frame);
+    loop {}
 }
